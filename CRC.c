e55be7185766936c86f707e578a550be6de84abf
Ganesh-kunta <k.ganesh1666@gmail.com>
2026-06-07 22:10:18 +0530

    hi

--- CRC.c
+++ CRC.c
@@ -1,86 +1,35 @@
-#include <stdio.h>
-#include <string.h>
-
-void xor_operation(char *dividend, char *divisor, int start, int n) {
-    for (int i = 0; i < n; i++) {
-        if (dividend[start + i] == divisor[i])
-            dividend[start + i] = '0';
-        else
-            dividend[start + i] = '1';
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char data[100], gen[100];
+    int i,j,g,d;
+    printf("enter the data:");
+    scanf("%s",data);
+    printf("enter the gen:");
+    scanf("%s",gen);
+    char original[100];
+    strcpy(original, data);
+    g = strlen(gen);
+    d = strlen(data);
+    for(i=0;i<g-1;i++){
+        data[d+i] = '0';
     }
-}
-
-void compute_crc(char data[], char divisor[]) {
-    int data_len = strlen(data);
-    int div_len = strlen(divisor);
-
-    char temp[100];
-
-    // Copy data to temp
-    strcpy(temp, data);
-
-    // Append zeros (n-1 zeros)
-    for (int i = 0; i < div_len - 1; i++) {
-        temp[data_len + i] = '0';
-    }
-    temp[data_len + div_len - 1] = '\0';
-
-    int total_len = strlen(temp);
-
-    // Division process
-    for (int i = 0; i <= total_len - div_len; i++) {
-        if (temp[i] == '1') {
-            xor_operation(temp, divisor, i, div_len);
+    data[d+g-1] = '\0';
+    for(i=0;i<d;i++){
+        if(data[i]=='1'){
+            for(j=0;j<g;j++){
+                data[i+j] = (data[i+j]==gen[j]) ? '0' : '1';
+            }
         }
     }
-
-    // Extract CRC (last n-1 bits)
-    char crc[20];
-    for (int i = 0; i < div_len - 1; i++) {
-        crc[i] = temp[total_len - (div_len - 1) + i];
+    printf("crc:");
+    for(i=d;i<d+g-1;i++){
+        printf("%c",data[i]);
     }
-    crc[div_len - 1] = '\0';
-
-    printf("\nCRC: %s", crc);
-
-    // Final transmitted data
-    printf("\nTransmitted Data: %s%s\n", data, crc);
+    printf("\nTransmitted frame: ");
+    printf("%s", original);   
+    for(i = d; i < d + g - 1; i++){
+    printf("%c", data[i]);
 }
-
-int main() {
-    char data[100];
-    int choice;
-
-    printf("Enter binary data: ");
-    scanf("%s", data);
-
-    printf("\nSelect CRC type:");
-    printf("\n1. CRC-12");
-    printf("\n2. CRC-16");
-    printf("\n3. CRC-CCITT\n");
-    printf("Enter choice: ");
-    scanf("%d", &choice);
-
-    char divisor[20];
-
-    switch (choice) {
-        case 1:
-            strcpy(divisor, "1101011011"); // CRC-12
-            break;
-        case 2:
-            strcpy(divisor, "11000000000000101"); // CRC-16
-            break;
-        case 3:
-            strcpy(divisor, "10001000000100001"); // CRC-CCITT
-            break;
-        default:
-            printf("Invalid choice!\n");
-            return 0;
-    }
-
-    printf("Using divisor: %s\n", divisor);
-
-    compute_crc(data, divisor);
-
     return 0;
 }
--- DVR.c
+++ DVR.c
@@ -1,23 +1,43 @@
 #include<stdio.h>
-
-int main() {
-    int n, cost[10][10];
-
-    printf("Enter number of nodes: ");
-    scanf("%d",&n);
-
+#define MAX 10
+#define INF 9999
+int main()
+{
+    int n, i, j, k;
+    int cost[MAX][MAX];
+    int dist[MAX][MAX];
+    printf("Enter no.of nodes:");
+    scanf("%d", &n);
     printf("Enter cost matrix:\n");
-    for(int i=0;i<n;i++)
-        for(int j=0;j<n;j++)
-            scanf("%d",&cost[i][j]);
-
-    printf("Routing Table:\n");
-
-    for(int i=0;i<n;i++) {
-        printf("Node %d:\n", i);
-        for(int j=0;j<n;j++) {
-            printf("To %d cost %d\n", j, cost[i][j]);
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            scanf("%d", &cost[i][j]);
+            dist[i][j] = cost[i][j];
         }
     }
+    for(k=0; k<n; k++)
+    {
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                if(dist[i][j] > dist[i][k] + dist[k][j])
+                {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    printf("\n shortest distance matrix:\n");
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            printf("%d", dist[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
-}
+} 
